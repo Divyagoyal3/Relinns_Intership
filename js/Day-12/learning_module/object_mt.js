577bb4e08567cda1a395fb72e68c44f651de0e99
Divyagoyal3 <goyaldivya631@gmail.com>
2024-01-14 18:07:19 +0530

    day -12

---
+++ js/Day-12/learning_module/object_mt.js
@@ -0,0 +1,16 @@
+// Creating an object
+let person = {
+  name: "John",
+  age: 30,
+};
+
+// Mutating the object by changing a property value
+person.age = 31;
+
+// Adding a new property
+person.gender = "Male";
+
+// Removing a property
+delete person.age;
+
+console.log(person);
