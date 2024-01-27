6ebffc4a74e7b397fa567d22587436124a2dda3b
Divyagoyal3 <goyaldivya631@gmail.com>
2024-01-27 17:38:51 +0530

    Day 3-4

--- angular/Day 3/OutputChild/src/app/app-Module/app-module.ts
+++ angular/Day 3/OutputChild/src/app/app-Module/app-module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { AppComponent } from '../app.component';
+import { EmployeeListComponent } from '../employee-list/employee-list.component';
+import { EmployeeDetailComponent } from '../employee-detail/employee-detail.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { EmployeeService } from '../employee.service';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AppComponent,EmployeeListComponent,EmployeeDetailComponent],
   imports: [
-    CommonModule
+    CommonModule,BrowserModule
   ]
 })
 export class YModule { }
