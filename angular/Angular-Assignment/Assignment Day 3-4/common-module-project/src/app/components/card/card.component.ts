8080d00675f255b9a25afaf1a4c22625c394c0cd
Divyagoyal3 <goyaldivya631@gmail.com>
2024-02-08 21:21:43 +0530

    assignment day 7-8

---
+++ angular/Angular-Assignment/Assignment Day 3-4/common-module-project/src/app/components/card/card.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+   title: string = 'Shoes';
+   description: string ="Shoes to buy ";
+   price: number = 7000;
+   ratings: number = 4.5/5 ;
+  
+}
